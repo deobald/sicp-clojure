beb8ea32e86de382b5dba88bcb225f3d898eff66
Steven Deobald <steven@deobald.ca>
2009-11-15 19:24:33 -0600

    2.29 c. + d.

--- src/sec2_2/ex2_29.clj
+++ src/sec2_2/ex2_29.clj
@@ -51,6 +51,32 @@
         right (make-branch 2 9)]
     (is (= 17 (total-weight (make-mobile left right))))))
 
+; c. Design a predicate that tests whether a binary mobile is balanced.
+
+(defn branch-torque [branch]
+  (* (branch-weight branch) (branch-length branch)))
+
+(defn balanced? [mobile]
+  (= (branch-torque (left-branch mobile))
+     (branch-torque (right-branch mobile))))
+
+(deftest a-mobile-is-balanced-if-both-branches-have-equal-torque
+  (let [left (make-branch 2 3)
+        right (make-branch 2 3)
+        mobile (make-mobile left right)]
+    (is (balanced? mobile))))
+
+(deftest a-mobile-is-not-balanced-if-one-branch-has-more-torque-than-the-other
+  (let [left (make-branch 2 3)
+        right (make-branch 2 4)
+        mobile (make-mobile left right)]
+    (is (not (balanced? mobile)))))
+
+; d. Suppose we change the constructors to use 'cons' (scheme). How much do you need to change the programs
+;    to convert to the new representation?
+
+; answer: only the selectors need to change, given a scheme-style cons. (car/cdr vs. car/cadr)
+
 (run-all-tests #"ex.*")
 
 
